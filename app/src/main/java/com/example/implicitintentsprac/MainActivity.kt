57e4ee7f9bf77df41418776ddf14160566193893
Hriday Keswani <hridaykeswani@gmail.com>
2022-07-13 17:31:37 +0530

    make UI and add onClickListener on Button

--- app/src/main/java/com/example/implicitintentsprac/MainActivity.kt
+++ app/src/main/java/com/example/implicitintentsprac/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.implicitintentsprac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener{
+            
+        }
     }
 }
